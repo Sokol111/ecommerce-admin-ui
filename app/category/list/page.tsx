824e35e84168e9345967fc3cc57b507efa2d6b6a
Sokol111 <igorsokol111@gmail.com>
2025-09-12 13:48:43 +0200

    modified

--- app/category/list/page.tsx
+++ app/category/list/page.tsx
@@ -1,10 +1,17 @@
 import AppCategoryList from "@/components/category/AppCategoryList";
+import { Button } from "@/components/ui/button";
 import { getAllCategories } from "@/lib/client/category-client";
+import Link from "next/link";
 
 export default async function CategotyListPage() {
   const categories = await getAllCategories();
   return (
     <div>
+      <div className="flex items-center gap-2 mb-5">
+        <Link href="/category/create">
+          <Button>Create category</Button>
+        </Link>
+      </div>
       <AppCategoryList categories={categories} />
     </div>
   );
