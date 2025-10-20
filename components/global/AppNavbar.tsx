d721637953c55a3ef261d05e4e3b49c63e1a3cbb
Sokol111 <igorsokol111@gmail.com>
2025-10-20 16:13:48 +0200

    updated

--- components/global/AppNavbar.tsx
+++ components/global/AppNavbar.tsx
@@ -1,3 +1,7 @@
 export default function AppNavbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <header className="flex sticky top-0 h-16 items-center border-b px-4 bg-white">
+      <h1 className="text-xl font-bold">Ecommerce Admin UI</h1>
+    </header>
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['imgproxy.127.0.0.1.nip.io'],
+  },
 };
 
 export default nextConfig;
