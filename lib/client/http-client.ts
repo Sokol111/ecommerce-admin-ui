3426cea7c6fafea7f898dc1669980fd8849dc376
Sokol111 <igorsokol111@gmail.com>
2025-12-11 19:36:52 +0100

    u

--- lib/client/http-client.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-
-/**
- * Creates an Axios instance for API calls
- */
-export function createHttpClient(config?: AxiosRequestConfig): AxiosInstance {
-  return axios.create(config);
-}
