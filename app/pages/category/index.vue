9d53a00198aa2abd51f5b029fe0da58af2ccc274
Sokol111 <igorsokol111@gmail.com>
2026-04-02 21:37:56 +0200

    u

--- app/pages/category/index.vue
+++ app/pages/category/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import type { TableColumn } from '@nuxt/ui';
-import type { CategoryResponse } from '@sokol111/ecommerce-catalog-service-api';
+import type { TableColumn } from '@nuxt/ui'
+import type { CategoryResponse } from '@sokol111/ecommerce-catalog-service-api'
 
 const {
   items,
