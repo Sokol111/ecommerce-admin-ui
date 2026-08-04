ce9c8f9bf4b17a671e1ae8102cf3d67080ffc316
Sokol111 <igorsokol111@gmail.com>
2026-08-04 20:19:09 +0200

    u

--- app/composables/useApiFetch.ts
+++ app/composables/useApiFetch.ts
@@ -2,8 +2,9 @@ export function useApiFetch<T = Record<string, unknown>>(
   request: Parameters<typeof useFetch<T>>[0],
   options?: Parameters<typeof useFetch<T>>[1]
 ) {
-  return useFetch<T>(request, {
-    ...options,
-    $fetch: useNuxtApp().$api
-  })
+  return useFetch<T>(request, Object.assign(
+    {},
+    options ?? {},
+    { $fetch: useNuxtApp().$api }
+  ))
 }
